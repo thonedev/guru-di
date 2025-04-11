9286dba84707a1a5c3eff7431b535a58811e9eda
Thone <thonecardoso@gmail.com>
2025-04-10 22:24:37 -0300

    commit inicial

---
+++ src/main/java/guru/th/di/GuruDiApplication.java
@@ -0,0 +1,13 @@
+package guru.th.di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/th/di/GuruDiApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.th.di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuruDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
