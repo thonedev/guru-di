c49057e6552cc30fe165bbce0618cc87c0475c64
Thone <thonecardoso@gmail.com>
2025-04-11 22:03:21 -0300

    injeção por profiles

--- src/test/java/guru/th/di/service/IHelloFromEnvironmentTest.java
+++ src/test/java/guru/th/di/service/IHelloFromEnvironmentTest.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-//@ActiveProfiles("qa")
+@ActiveProfiles("qa")
 @SpringBootTest
 class IHelloFromEnvironmentTest {
 
